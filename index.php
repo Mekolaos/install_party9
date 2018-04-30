5d91515fd83c08d769672f7ded2da5881a4b47ce
Mekolaos <31978152+Mekolaos@users.noreply.github.com>
2018-04-30 21:51:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -531,7 +531,7 @@ setTimeout(function(){ saske.style.display="none";}, 5000);};
 
 <script src="ip_js/jquery-3.3.1.js"></script>
 <script src="ip_js/bootstrap.min.js"></script>
-<script src="ip_js/jquery.min.js"></script>
+<script src="lightbox/jquery.min.js"></script>
 <script src="owlcarousel/owl.carousel.min.js"></script>
 <script src="lightbox/lity.min.js"></script>
 <script src="lightbox/jquery.js"></script>
